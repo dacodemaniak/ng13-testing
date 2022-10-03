8786bcc8487d465f1e5168731fd465c01860287c
DaCodeManiaK <jla.webprojet@gmail.com>
2022-10-03 15:17:25 +0200

    Update test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { InternInitialPipe } from './shared/pipes/intern-initial.pipe';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        InternInitialPipe
       ],
     }).compileComponents();
   });
