ee97b63c8535c885d48779a53c1849960189fdd5
DaCodeManiaK <jla.webprojet@gmail.com>
2022-10-05 10:05:19 +0200

    UPDATE: Add input as directive name

--- src/app/shared/directives/is-required.directive.spec.ts
+++ src/app/shared/directives/is-required.directive.spec.ts
@@ -1,8 +1,73 @@
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { IsRequiredDirective } from './is-required.directive';
 
+/**
+ * Let's create a dummy component for testing only
+ */
+ @Component({
+  template: `<input type="text" placeholder="Type your name..." appIsRequired>`
+})
+class TestComponent {}
+
+@Component({
+  template: `<input type="text" appIsRequired>`
+})
+class NoPlaceholderComponent {}
+
 describe('IsRequiredDirective', () => {
-  it('should create an instance', () => {
-    const directive = new IsRequiredDirective();
-    expect(directive).toBeTruthy();
+  let testComponent: TestComponent;
+  let testFixture: ComponentFixture<TestComponent>;
+
+  let nopComponent: NoPlaceholderComponent;
+  let nopFixture: ComponentFixture<NoPlaceholderComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        TestComponent,
+        NoPlaceholderComponent,
+        IsRequiredDirective
+      ]
+    });
+
+    testFixture = TestBed.createComponent(TestComponent);
+    testComponent = testFixture.componentInstance;
+
+    nopFixture = TestBed.createComponent(NoPlaceholderComponent);
+    nopComponent = nopFixture.componentInstance;
+  });
+
+  it(`Should render 'Type your name...(*)' in placeholder attribute`, () => {
+    // Play a detection changes cycle
+    testFixture.detectChanges();
+
+    // First get the element via debugElement
+    const debugElement: HTMLElement = testFixture.debugElement.nativeElement;
+    const input: HTMLElement = debugElement.querySelector('input')!;
+
+    // Get the placeholder attribute content
+    const placeholderContent: string = input.getAttribute('placeholder')!;
+
+    expect(placeholderContent).toEqual('Type your name...(*)');
+  });
+
+  it(`Should add placeholder attribute and render 'Type something required... (*)' in that placeholder`, () => {
+    // Play a detection changes cycle
+    nopFixture.detectChanges();
+
+
+    // First get the element via debugElement
+    const debugElement: HTMLElement = nopFixture.debugElement.nativeElement;
+    const input: HTMLElement = debugElement.querySelector('input')!;
+
+    // Expect input had a placeholder attribute
+    const hasPlaceHolder: boolean = input.getAttribute('placeholder') !== null;
+    expect(hasPlaceHolder).toBeTrue();
+
+    // Get the placeholder attribute content
+    const placeholderContent: string = input.getAttribute('placeholder')!;
+
+    expect(placeholderContent).toEqual('Type something required... (*)');
   });
 });
--- src/app/shared/directives/is-required.directive.ts
+++ src/app/shared/directives/is-required.directive.ts
@@ -1,20 +1,27 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appIsRequired]'
 })
 export class IsRequiredDirective implements OnInit {
+  @Input() public appIsRequired: string | null = null;
 
-  constructor(private elementRef: ElementRef) { }
+  constructor(private elementRef: ElementRef) {}
+
+  
 
   public ngOnInit(): void {
+    if (this.appIsRequired?.length === 0) {
+      this.appIsRequired = '*';
+    }
+    
     let newPlaceholderContent: string;
 
       const nativeElement: HTMLElement = this.elementRef.nativeElement;
       if (nativeElement.getAttribute('placeholder')) {
-        newPlaceholderContent = nativeElement.getAttribute('placeholder') + '(*)';
+        newPlaceholderContent = nativeElement.getAttribute('placeholder') + '(' + this.appIsRequired + ')';
       } else {
-        newPlaceholderContent = 'Type something required... (*)';
+        newPlaceholderContent = 'Type something required... ' + '(' + this.appIsRequired + ')';
       }
 
       // At least, replace the content of attribute
